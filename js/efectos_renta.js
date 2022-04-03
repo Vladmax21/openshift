053c59ae40b456fabe91a9f7e4e1bee54df5a83a
vlady18 <vladymir07h@gmail.com>
2022-04-03 01:32:14 -0600

    va

---
+++ js/efectos_renta.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+// Scroll Elementos Menu
+	var compactacion = $('#compactacion').offset().top,
+		concreto = $('#concreto').offset().top,
+		corte = $('#corte').offset().top,
+		electricidad = $('#electricidad').offset().top;
+		elevacion = $('#elevacion').offset().top;
+		multitarea = $('#multitarea').offset().top;
+
+// Boton Acerca de
+	$('#btn-compactacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 380
+		}, 500);
+	});
+
+	$('#btn-concreto').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: concreto - 40
+		});
+	});
+
+	$('#btn-corte').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: corte - 40
+		});
+	});
+
+	$('#btn-electricidad').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: electricidad - 40
+		});
+	});
+
+	$('#btn-elevacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: elevacion - 40
+		});
+	});
+
+	$('#btn-multitarea').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: multitarea - 40
+		});
+	});
+
+
+
+});
